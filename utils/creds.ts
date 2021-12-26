5c85300e314760cbfef8c56ac88e305519a8731d
James Volpe <jamesvolpe@MacBook-Pro.local>
2021-12-25 18:52:15 -0800

    fix dbclient init

--- utils/creds.ts
+++ utils/creds.ts
@@ -1,12 +1,13 @@
 import AWS from "aws-sdk";
 import { fromSSO } from "@aws-sdk/credential-provider-sso";
 
-export const dbCredentials = await fromSSO({
-  ssoStartUrl: process.env.SSO_START_URL,
-  ssoAccountId: process.env.SSO_ACCOUNT_ID,
-  ssoRegion: "us-east-2",
-  ssoRoleName: "DatabaseAdministrator",
-})();
-
-export const db = new AWS.DynamoDB({ credentials: dbCredentials });
-export const dbClient = new AWS.DynamoDB.DocumentClient({ service: db });
+export const dbClient = async () => {
+  const dbCreds = await fromSSO({
+    ssoStartUrl: process.env.SSO_START_URL,
+    ssoAccountId: process.env.SSO_ACCOUNT_ID,
+    ssoRegion: "us-east-2",
+    ssoRoleName: "DatabaseAdministrator",
+  })();
+  const db = new AWS.DynamoDB({ credentials: dbCreds });
+  return new AWS.DynamoDB.DocumentClient({ service: db });
+};
