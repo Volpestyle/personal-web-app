97c9e23cfe0eb28d3e55b8e96b2c3cea3b798966
James Volpe <jamesvolpe@MacBook-Pro.local>
2021-12-25 19:08:38 -0800

    top-level await

--- utils/creds.ts
+++ utils/creds.ts
@@ -1,13 +1,12 @@
 import AWS from "aws-sdk";
 import { fromSSO } from "@aws-sdk/credential-provider-sso";
 
-export const dbClient = async () => {
-  const dbCreds = await fromSSO({
-    ssoStartUrl: process.env.SSO_START_URL,
-    ssoAccountId: process.env.SSO_ACCOUNT_ID,
-    ssoRegion: "us-east-2",
-    ssoRoleName: "DatabaseAdministrator",
-  })();
-  const db = new AWS.DynamoDB({ credentials: dbCreds });
-  return new AWS.DynamoDB.DocumentClient({ service: db });
-};
+export const dbCredentials = await fromSSO({
+  ssoStartUrl: process.env.SSO_START_URL,
+  ssoAccountId: process.env.SSO_ACCOUNT_ID,
+  ssoRegion: "us-east-2",
+  ssoRoleName: "DatabaseAdministrator",
+})();
+
+export const db = new AWS.DynamoDB({ credentials: dbCredentials });
+export const dbClient = new AWS.DynamoDB.DocumentClient({ service: db });
