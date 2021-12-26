f7db112fb562a7e1148b3714e44fb74fbafd8c75
James Volpe <jamesvolpe@MacBook-Pro.local>
2021-12-26 15:15:58 -0800

    next env vars

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SSO_ENTRY_POINT: process.env.SSO_ENTRY_POINT,
+    SSO_EXIT_POINT: process.env.SSO_EXIT_POINT,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 };
