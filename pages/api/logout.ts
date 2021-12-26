26bdc348966afe0248803f1c33f5a1913c47bb04
James Volpe <jamesvolpe@MacBook-Pro.local>
2021-12-26 15:12:37 -0800

    fix logout api

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,12 +1,12 @@
 import { NextApiResponse, NextApiRequest } from "next";
-import { sp, idp } from "../../utils/samlProviders";
+import samlProviders from "../../utils/samlProviders";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { sp, idp } = await samlProviders();
   var options = {
     name_id: "name_id",
     session_index: "session_index",
   };
-
   sp.create_logout_request_url(idp, options, (err: any, logout_url: string) => {
     if (err != null) return res.send(500);
     res.redirect(logout_url);
