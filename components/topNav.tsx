ab965175d417f75bc9c55bb570473d90c928afb1
James Volpe <jamesvolpe@MacBook-Pro.local>
2021-12-27 18:13:06 -0800

    clean up

--- components/topNav.tsx
+++ components/topNav.tsx
@@ -1,13 +1,10 @@
 import { signOut } from "next-auth/react";
 import { useToasts } from "react-toast-notifications";
 import styles from "../styles/components/topnav.module.scss";
-import { errorOptions, successOptions } from "../utils/toasts";
 import { Button } from "./button";
 import { useCMSContext } from "./providers/cmsProvider";
 
 export const TopNav = () => {
-  const { addToast } = useToasts();
-  const { user } = useCMSContext();
   const handleLogout = () => {
     signOut({ callbackUrl: `${window.location.pathname}/login` });
   };
